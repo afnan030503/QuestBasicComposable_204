d9ae1983cf57be6bc27df229b1cde7cedfcdd87a
afnan030503 <muhammadafnanmaulidi@gmail.com>
2024-10-17 22:41:09 +0700

    mengubah menjadi basic column supaya

---
+++ app/src/main/java/com/example/questbasiccomposable_204/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.questbasiccomposable_204
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalMapOf
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.questbasiccomposable_204.ui.theme.QuestBasicComposable_204Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            QuestBasicComposable_204Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    BasicColumn(modifier = Modifier.padding(innerPadding))
+
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    QuestBasicComposable_204Theme {
+        Greeting("Android")
+    }
+}
