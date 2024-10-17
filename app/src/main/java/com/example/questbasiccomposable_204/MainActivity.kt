c211f79ef8e51fa16d8b1e403b29dcf301c28af4
afnan030503 <muhammadafnanmaulidi@gmail.com>
2024-10-17 23:07:13 +0700

    mengatur bagian image

--- app/src/main/java/com/example/questbasiccomposable_204/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable_204/MainActivity.kt
@@ -10,11 +10,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.modifier.modifierLocalMapOf
 import androidx.compose.ui.res.painterResource
@@ -68,7 +70,10 @@ fun BasicColumn(modifier: Modifier = Modifier) {
         Spacer(modifier = Modifier.padding(20.dp))
         Image(
 
-            painter = painterResource(id = R.drawable.logo_universitas)
+            painter = painterResource(id = R.drawable.logo_universitas),
+            contentDescription = "Logo Universitas",
+            modifier = Modifier
+                .clip(CircleShape)
         )
     }
 }
