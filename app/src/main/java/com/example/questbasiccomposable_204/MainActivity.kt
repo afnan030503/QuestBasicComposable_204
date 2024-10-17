6ef04827550100efc2869bd6d4c073d11f800520
afnan030503 <muhammadafnanmaulidi@gmail.com>
2024-10-17 23:00:31 +0700

    mengatur text, size, style dan warna

--- app/src/main/java/com/example/questbasiccomposable_204/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable_204/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -13,10 +14,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.modifier.modifierLocalMapOf
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.questbasiccomposable_204.ui.theme.QuestBasicComposable_204Theme
+import org.w3c.dom.Text
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,7 +41,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun BasicColumn(modifier: Modifier = Modifier) {
-    Column {  }(
+    Column(
        horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangment = Arrangement.Top,
         modifier = modifier
@@ -46,6 +52,15 @@ fun BasicColumn(modifier: Modifier = Modifier) {
         Text(
             text = "Login",
             fontSize = 50.dp,
+            fontWeight = FontWeight.Bold
+        )
+
+        Spacer(modifier = Modifier.padding(4.dp))
+        Text(
+            text = "Ini adalah halamn login",
+            fontSize = 20.sp,
+            fontStyle = FontStyle.Italic,
+            color = Color.Gray
         )
     }
 }
